9c618b68358fb94b92763406dd63048d2115d3bb
Thu <thub2012152@student.ctu.edu.vn>
2024-05-23 22:17:21 +0700

    23/5/2024

---
+++ src/views/SignUp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid mt-0">
+    <div
+      class="min-vh-80 pt-5 pb-11 col-sm-12"
+      style="
+        background-image: url('../../public/4-3.jpg');
+        background-position: top;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 400px;
+      "
+    >
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="">
+        <div class="row justify-content-center">
+          <div class="col-sm-5 text-center mx-auto">
+            <h1 class="text-white mb-2 mt-5">Welcome!</h1>
+            <p class="text-lead text-white">Transaction Management</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
